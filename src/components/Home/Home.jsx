aaf6b6040c97fda37864455559889b9c928d1df6
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2021-12-30 01:59:18 +0530

    define default movies & shows

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,9 +6,11 @@ import { fetchAsyncMovies, fetchAsyncShows } from '../../features/movies/movieSl
 const Home = () => {
 
     const dispatch = useDispatch(); //create dispatch
+    const movieText = "X-men"; // set default values
+    const showText = "Loki";
     useEffect(() => {
-        dispatch(fetchAsyncMovies()); //call the function
-        dispatch(fetchAsyncShows());
+        dispatch(fetchAsyncMovies(movieText)); //fetch the default movies list
+        dispatch(fetchAsyncShows(showText));// fetch the default tv shows
     },[dispatch]);
     return (
         <div>
