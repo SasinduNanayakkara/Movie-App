bb30d6e8f93b6333e3f5acb53f23cd146d964c32
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2021-12-30 02:00:51 +0530

    add react slick carousel

--- src/components/MovieListing/MovieListing.jsx
+++ src/components/MovieListing/MovieListing.jsx
@@ -2,27 +2,32 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { getAllMovies, getAllShows } from '../../features/movies/movieSlice'
 import MovieCard from '../MovieCard/MovieCard';
+import Slider from "react-slick";
+import {settings} from "../../common/setting";
 import "./MovieListing.scss" 
 const MovieListing = () => {
     
-    const movies = useSelector(getAllMovies);
-    const shows = useSelector(getAllShows);
+    const movies = useSelector(getAllMovies); //get all movie data
+    const shows = useSelector(getAllShows);// get all show data
     let renderMovies = "";
     let renderShows = "";
 
+    //map the movie data
     renderMovies = movies.Response === "True" ? (
         movies.Search.map((movie, index) =>(
             <MovieCard key={index} data={movie} />
         ))
     ) : (
-        <div className='movies-error'>
+
+        <div className='movies-error'> 
             <h3>{movies.Error}</h3>
         </div>
     );
-
+    //map the show data
     renderShows = shows.Response === "True" ? (
         shows.Search.map((movie, index) =>(
             <MovieCard key={index} data={movie} />
+            
         ))
     ) : (
         <div className='shows-error'>
@@ -36,13 +41,13 @@ const MovieListing = () => {
             <div className="movie-list">
                 <h2>Movies</h2>
                 <div className="movie-container">
-                    {renderMovies}
+                    <Slider {...settings}>{renderMovies}</Slider> {/*render date carouselly */}
                 </div>
             </div>
             <div className="show-list">
                 <h2>Shows</h2>
                 <div className="movie-container">
-                    {renderShows}
+                    <Slider {...settings}>{renderShows}</Slider>
                 </div>
             </div>
         </div>
